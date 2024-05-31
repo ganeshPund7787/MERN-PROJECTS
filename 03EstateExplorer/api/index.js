9cb49be04f477a50be359b7ceab58490b52d1441
Ganesh Pund <ganupund7787@gmail.com>
2024-05-31 08:14:13 -0700

    connect mongoDB atlas

--- 03EstateExplorer/api/index.js
+++ 03EstateExplorer/api/index.js
@@ -1,10 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log(`MongoDb connected `))
+    .catch(() => console.log(`Error while mongoDB : ${console.error()}`))
 
 const app = express();
 
 
 app.listen(3000, () => {
-    console.log(`The port is running on 3000 !`)
+    console.log(`The port is running on 3000!`)
 });
 
 
