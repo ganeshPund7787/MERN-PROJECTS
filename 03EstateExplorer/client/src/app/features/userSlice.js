27b5899643b957c921ba7190f6c52a5f5d367107
Ganesh Pund <ganupund7787@gmail.com>
2024-06-01 08:12:21 -0700

    add redux

---
+++ 03EstateExplorer/client/src/app/features/userSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    currentUser: null,
+    error: null,
+    loading: false
+}
+
+const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        signInStart: (state, action) => {
+            state.loading = true;
+        },
+        signInSuccess: (state, action) => {
+            state.loading = false
+            state.error = null;
+            state.currentUser = action.payload;
+            // localStorage.setItem = (JSON.stringify(action.payload))
+        },
+        signInFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        }
+    }
+})
+
+
+export const { signInFailure, signInStart, signInSuccess } = userSlice.actions;
+
+export default userSlice.reducer;
