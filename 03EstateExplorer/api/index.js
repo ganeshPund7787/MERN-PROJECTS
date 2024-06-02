8794b5b8cc1742d601c49f2c39f7cbfa7cd97a84
Ganesh Pund <ganupund7787@gmail.com>
2024-06-02 14:58:35 -0700

    craete listing api

--- 03EstateExplorer/api/index.js
+++ 03EstateExplorer/api/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv"
 dotenv.config();
 import userRouter from "./routes/user.routes.js"
 import authRouter from "./routes/auth.route.js"
+import listingRouter from "./routes/listing.route.js"
 import cookieParser from "cookie-parser";
 
 mongoose.connect(process.env.MONGO_URI, { dbName: "estateExplorer" })
@@ -21,6 +22,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
