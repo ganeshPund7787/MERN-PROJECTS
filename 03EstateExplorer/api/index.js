ce9f561194e72fe9a2d44ec26f2fe7cafc602103
Ganesh Pund <ganupund7787@gmail.com>
2024-06-07 06:01:43 -0700

    pre installation changes

--- 03EstateExplorer/api/index.js
+++ 03EstateExplorer/api/index.js
@@ -6,10 +6,13 @@ import userRouter from "./routes/user.routes.js"
 import authRouter from "./routes/auth.route.js"
 import listingRouter from "./routes/listing.route.js"
 import cookieParser from "cookie-parser";
+import path from "path"
 
 mongoose.connect(process.env.MONGO_URI, { dbName: "estateExplorer" })
     .then(() => console.log(`MongoDb connected `))
     .catch((error) => console.log(`Error while mongoDB : ${error}`))
+
+const __dirname = path.resolve();
 
 const app = express();
 app.use(cookieParser())
@@ -23,6 +26,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
--- 03EstateExplorer/client/README.md
+++ 03EstateExplorer/client/README.md
@@ -9,3 +9,13 @@
 # use regax mongoose function for find query
 
 # advanced Authentication with jwt-token
+
+## Deploying Steps:
+
+    1) Changes In Package.json
+        > cmd
+    2) Changes In index.js on server side
+        > import pat
+        > join path
+        > Allow any IP address
+    3)
