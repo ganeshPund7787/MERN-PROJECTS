df59882d3751c4cf6c7e084845ae8388cd2e3214
Ganesh Pund <ganupund7787@gmail.com>
2024-06-09 13:03:12 -0700

    add tailwind css

--- 04ChatApp/API/controllers/user.controllers.js
+++ 04ChatApp/API/controllers/user.controllers.js
@@ -3,7 +3,7 @@ import { User } from "../models/user.models.js";
 export const getUserForSidebar = async (req, res, next) => {
     try {
         const loggedInUserId = req.user._id;
-        const filterUsers = await User.find({ _id: { $ne: loggedInUserId } });
+        const filterUsers = await User.find({ _id: { $ne: loggedInUserId } }).select("-password");
         res.status(200).json(filterUsers);
     } catch (error) {
         console.log(`Error while get Side Bar usrs : ${error.message}`)
