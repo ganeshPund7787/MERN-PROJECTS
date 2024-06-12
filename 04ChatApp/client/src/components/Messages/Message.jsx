649cbe28c167abb1faf0a09ad9155b785f0e863f
Ganesh Pund <ganupund7787@gmail.com>
2024-06-12 08:16:42 -0700

    add send message functionality

--- 04ChatApp/client/src/components/Messages/Message.jsx
+++ 04ChatApp/client/src/components/Messages/Message.jsx
@@ -5,16 +5,23 @@ import useConversation from "../../zustand/useConversation";
 const Message = ({ message }) => {
   const { authUser } = useAuthContex();
   const { selectedConversation } = useConversation();
+  const fromMe = message.senderId === authUser._id;
+  const chatClassName = fromMe ? "chat-end" : "chat-start";
+  const profilePic = fromMe
+    ? authUser.profilePic
+    : selectedConversation.profilePic;
+  const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+
   return (
-    <div className="chat chat-end">
+    <div className={`chat ${chatClassName}`}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
-          <img src="https://avatar.iran.liara.run/public/boy?" alt="...." />
+          <img src={profilePic} alt="...." />
         </div>
       </div>
 
-      <div className={`chat-bubble text-white bg-blue-500`}>
-        Hii! What's Up ?
+      <div className={`chat-bubble text-white ${bubbleBgColor}`}>
+        {message.message}
       </div>
       <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
         12:42
