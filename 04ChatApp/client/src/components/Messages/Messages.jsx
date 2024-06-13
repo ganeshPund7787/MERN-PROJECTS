f3aad79a3ad1a7606407e2a8eeebbcf4c7d4c636
Ganesh Pund <ganupund7787@gmail.com>
2024-06-13 07:35:24 -0700

    add live message send functionality

--- 04ChatApp/client/src/components/Messages/Messages.jsx
+++ 04ChatApp/client/src/components/Messages/Messages.jsx
@@ -2,9 +2,11 @@ import React, { useEffect, useRef } from "react";
 import Message from "./Message";
 import useGetMessages from "../../Hooks/useGetMessages";
 import MessageSkeletons from "../skeletons/MessageSkeletons";
+import useListenMessages from "../../Hooks/useListenMessages";
 
 const Messages = () => {
   const { messages, loading } = useGetMessages();
+  useListenMessages();
   const lastMessageRef = useRef();
 
   useEffect(() => {
