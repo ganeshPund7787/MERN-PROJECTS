24d8700981739d38cfc9e3e69fa47e43f5d3d79c
Ganesh Pund <ganupund7787@gmail.com>
2024-06-13 07:47:59 -0700

    add message notification sound

--- 04ChatApp/client/src/Hooks/useListenMessages.js
+++ 04ChatApp/client/src/Hooks/useListenMessages.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 import { useSocketContext } from '../context/SocketContext'
 import useConversation from '../zustand/useConversation'
 
+import notification from "../assets/sounds/notification.mp3"
+
 const useListenMessages = () => {
     const { socket } = useSocketContext()
     const { messages, setMessages } = useConversation()
@@ -9,6 +11,8 @@ const useListenMessages = () => {
     useEffect(() => {
         socket?.on("newMessage", (newMessage) => {
             newMessage.shouldShake = true;
+            const sound = new Audio(notification)
+            sound.play();
             setMessages([...messages, newMessage]);
         })
 
